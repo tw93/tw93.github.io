362936d1a32080773c73fd04031d8af52b660689
tw93 <tw93@qq.com>
2017-01-22 12:03:07 +0800

    modify js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", function(){
   window.onscroll = function () {
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
       document.getElementById("J_header").setAttribute('class', 'header-menu header-menu-overflow');
     } else {
       document.getElementById("J_header").setAttribute('class', 'header-menu header-menu-top');
