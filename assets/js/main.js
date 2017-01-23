0b5425160d2ba334cdd338d10941d2ead8232fd3
tw93 <tw93@qq.com>
2017-01-23 09:42:03 +0800

    change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-  if (/\(i[^;]+;( U;)? CPU.+Mac OS X/.test(navigator.userAgent)) {
-    document.addEventListener("gesturechange", dealHeadShow, true);
-  } else {
-    document.addEventListener("scroll", dealHeadShow, false);
-  }
+  document.addEventListener("gesturechange", dealHeadShow);
+  document.addEventListener("scroll", dealHeadShow);
 }, false);
 
 function dealHeadShow() {
-  console.log(1)
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
     document.getElementById("J_header").setAttribute('class', 'header-menu header-menu-overflow');
   } else {
